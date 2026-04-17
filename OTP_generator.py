05406975ea4ad34e61fb80096ec544b26127590b
Ayush Singh <ayush893singh@gmail.com>
2026-04-17 22:43:39 +0530

    Add files via upload

---
+++ OTP_generator.py
@@ -0,0 +1,12 @@
+
+import random
+
+otp = random.randint(100000, 999999)
+print("Your OTP is:", otp)
+
+user_otp = int(input("Enter OTP: "))
+
+if user_otp == otp:
+    print("OTP Verified!")
+else:
+    print("Wrong OTP!")
